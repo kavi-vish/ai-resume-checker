a8ce52e06252b8d5314990c324b67fc78b583a90
Kavindu Vishwajith Jayakodi <kvishwajith2@gmail.com>
2026-03-28 16:06:56 -0600

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Rescheck" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section" >
+      <div className="page-heading" >
+        <h1>AI-Powered Resume Checker That Gets You Hired Faster</h1>
+        <h2>Analyze your resume in seconds. Get smart suggestions, improve ATS score, and stand out to recruiters.</h2>
+      </div>
+    </section>
+    {resumes.length>0 &&(
+    <div className="resumes-section">
+      {resumes.map((resume, index) => (
+          <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+    )}
+  </main>
 }
